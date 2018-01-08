3f78c8f7f450183f59305985dbc4c7ceb3494dab
csabal <csaba.lep@gmail.com>
2018-01-08 10:20:25 +0100

    rename parameters remove not used parameter

--- poco/poco_repo.py
+++ poco/poco_repo.py
@@ -4,13 +4,13 @@ class PocoRepo:
 
     DEFAULT = """Poco repo commands
     Usage:
-      poco repo add <catalog> <git-url> [<branch>] [<file>]
-      poco repo branch <branch> [<catalog>] [-f]
-      poco repo branches [<catalog>]
+      poco repo add <name> <git-url> [<branch>] [<file>]
+      poco repo branch <branch> [<name>] [-f]
+      poco repo branches [<name>]
       poco repo init
       poco repo ls
-      poco repo push [<catalog>]
-      poco repo (remove|rm) <catalog>
+      poco repo push [<name>]
+      poco repo (remove|rm) <name>
     """
 
     INIT = """Usage:
@@ -30,61 +30,61 @@ class PocoRepo:
     """
 
     ADD = """Usage:
-      poco repo add <catalog> <git-url> [<branch>] [<file>]
+      poco repo (add|modify) <name> <git-url> [<branch>] [<file>]
 
         -h, --help
 
         Specific parameters:
-            <catalog>       Name of the catalogue.
+            <name>          Name of the catalogue.
             <git-url>       URL for catalogue's GIT repository.
             <branch>        Name of the branch what we want to set. (default : master)
             <file>          Name of the catalogue file in the repository. (default: poco-catalog.yml)
 
-        Add new catalogue to the config.
+        Add/Modify new catalogue to the config.
     """
 
     REMOVE = """Usage:
-      poco repo (remove|rm) <catalog>
+      poco repo (remove|rm) <name>
 
         -h, --help
 
         Specific parameters:
-            <catalog>       Name of the catalogue.
+            <name>       Name of the catalogue.
 
         Remove catalogue from local config.
     """
 
     BRANCH = """Usage:
-        poco repo branch <branch> [<catalog>] [-f]
+        poco repo branch <branch> [<name>] [-f]
 
         -h, --help
 
         Specific parameters:
             <branch>        Name of the branch what we want to set.
-            <catalog>       Name of the catalogue.
+            <name>          Name of the catalogue.
             -f              Force switch.
 
         Switch catalogue branch if its using GIT.
     """
 
     BRANCHES = """Usage:
-        poco repo branches [<catalog>]
+        poco repo branches [<name>]
 
         -h, --help
 
         Specific parameters:
-            <catalog>       Name of the catalogue.
+            <name>       Name of the catalogue.
 
         List all available branch in catalogue's GIT repository.
     """
 
     PUSH = """Usage:
-        poco repo push [<catalog>]
+        poco repo push [<name>]
 
         -h, --help
 
         Specific parameters:
-            <catalog>       Name of the catalogue.
+            <name>       Name of the catalogue.
 
         Push changes into catalogue's remote GIT repository
     """
@@ -93,6 +93,7 @@ class PocoRepo:
         'init': INIT,
         'ls': LS,
         'add': ADD,
+        'modify': ADD,
         'remove': REMOVE,
         'rm': REMOVE,
         'branch': BRANCH,
