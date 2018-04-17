1cae09154abf1cf8d4164cb80cfe193396ac7a66
csabal <csaba.lep@gmail.com>
2018-04-17 12:37:10 +0200

    probably fixed travis fail on deploy new version

--- pocok/pocok.py
+++ pocok/pocok.py
@@ -29,7 +29,7 @@ from .services.state import StateHolder
 
 
 END_STRING = """See 'pocok help <command>' for more information on a specific command."""
-__version__ = '0.90.5'
+__version__ = '0.90.6'
 
 
 class Pocok(object):
