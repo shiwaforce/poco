db3c94fc11749fd3f0a161ebd2284deaa93e5a42
csabal <csaba.lep@gmail.com>
2018-05-24 15:40:42 +0200

    new version

--- proco/proco.py
+++ proco/proco.py
@@ -29,7 +29,7 @@ from .services.state import StateHolder
 
 
 END_STRING = """See 'proco help <command>' for more information on a specific command."""
-__version__ = '0.95.0'
+__version__ = '0.95.1'
 
 
 class Proco(object):
