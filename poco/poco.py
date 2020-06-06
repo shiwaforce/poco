6dd015f78cef3cffdca2fc4f0726b3be11c16130
sarkiroka <sarki_roka@freemail.hu>
2020-06-06 17:04:54 +0200

    new version

--- poco/poco.py
+++ poco/poco.py
@@ -29,7 +29,7 @@ from .services.state import StateHolder
 
 
 END_STRING = """See 'poco help <command>' for more information on a specific command."""
-__version__ = '0.96.9'
+__version__ = '0.96.10'
 
 
 class Poco(object):
