d71837fc35f034c5765d89be621b94b3d1ee5f3e
laszlolevi <laszlolevi@icloud.com>
2022-08-04 13:19:16 +0200

    0.99.2.dev1

--- poco/poco.py
+++ poco/poco.py
@@ -32,7 +32,7 @@ from .services.state import StateHolder
 
 
 END_STRING = """See 'poco help <command>' for more information on a specific command."""
-__version__ = '0.99.1'
+__version__ = '0.99.2.dev1'
 
 
 class Poco(object):
